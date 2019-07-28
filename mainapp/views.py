3130d37726f3692cec485ad190f52c6ad2c7aae8
sttm <nikitasavenkov7@gmail.com>
2019-07-29 02:41:11 +0300

    add readme

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 
 
@@ -9,8 +10,36 @@ def index(request):
 
 
 def contact(request):
+    locations = [
+        {
+            'city': 'One',
+            'phone': '+7-900-800-70-60',
+            'email': 'info@geekshop.inbox',
+            'address': 'address Street 1',
+        },
+        {
+            'city': 'Two',
+            'phone': '+7-800-700-60-50',
+            'email': 'info@geekshop.inbox',
+            'address': 'address Street 2',
+        },
+        {
+            'city': 'Three',
+            'phone': '+7-700-600-50-40',
+            'email': 'info@geekshop.inbox',
+            'address': 'address Street 3',
+        },
+    ]
+
+    # with open('geekshop/locations.json', 'w', encoding='utf-8') as f:
+    #     json.dump(locations, f)
+
+    # with open('geekshop/locations.json', 'r', encoding='utf-8') as f:
+    #     locations = json.load(f)
+
     context = {
         'page_title': 'contact',
+        'locations': locations,
     }
     return render(request, 'mainapp/contact.html', context)
 
